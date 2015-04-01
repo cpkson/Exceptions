e42cca62db96d30749ff30827685c49ee9488ad1
craigp <craigp@travelplaces.co.uk>
2015-04-01 15:22:35 +0100

    Initial Commit

---
+++ src/main/java/co/uk/travelplaces/exception/ProcessingException.java
@@ -0,0 +1,45 @@
+package co.uk.travelplaces.exception;
+
+/**
+ * Processing Exceptions
+ * 
+ * @author craigp
+ * @since 27/2/2015
+ * @version 0.1
+ */
+public class ProcessingException extends Exception
+{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4354187063995251541L;
+
+	/**
+	 * 
+	 * @param message
+	 */
+	public ProcessingException(String message)
+	{
+		super(message);
+	}
+	
+	/**
+	 * 
+	 * @param message
+	 * @param throwable
+	 */
+	public ProcessingException(String message, Throwable throwable)
+	{
+		super(message, throwable);
+	}
+	
+	/**
+	 * 
+	 * @param throwable
+	 */
+	public ProcessingException(Throwable throwable)
+	{
+		super(throwable);
+	}
+}
